2ce93a7ea8b6737b25484906c26604a5db917a7b
Victor Poughon <victor.poughon@gmail.com.com>
2025-01-09 17:43:37 +0100

    add types to physics.py

--- src/torchlensmaker/physics.py
+++ src/torchlensmaker/physics.py
@@ -1,7 +1,11 @@
 import torch
+from typing import Literal
+
+Tensor = torch.Tensor
+RefractionCriticalAngleMode = Literal["drop", "nan", "clamp"]
 
 
-def reflection(rays, normals):
+def reflection(rays: Tensor, normals: Tensor) -> Tensor:
     """
     Vector based reflection.
 
@@ -15,16 +19,22 @@ def reflection(rays, normals):
 
     dot_product = torch.sum(rays * normals, dim=1, keepdim=True)
     R = rays - 2 * dot_product * normals
-    return R / torch.norm(R, dim=1, keepdim=True)
+    return torch.div(R, torch.norm(R, dim=1, keepdim=True))
 
 
-def refraction(ray, normal, n1, n2, critical_angle='drop'):
+def refraction(
+    ray: Tensor,
+    normal: Tensor,
+    n1: float,
+    n2: float,
+    critical_angle: RefractionCriticalAngleMode = "drop",
+) -> Tensor:
     """
     Vector based refraction (Snell's law).
-    
+
     The 'critical_angle' argument specifies how incident rays beyond the
     critical angle are handled:
-    
+
         * 'nan': Incident rays beyond the critical angle will refract
           as nan values. The returned tensor always has the same shape as the
           input tensors.
@@ -42,7 +52,7 @@ def refraction(ray, normal, n1, n2, critical_angle='drop'):
         n1: index of refraction of the incident medium (float)
         n2: index of refraction of the refracted medium (float)
         critical_angle: one of 'nan', 'clamp', 'drop' (default: 'nan')
-    
+
     Returns:
         unit vectors of the refracted rays, shape (C, 2)
     """
@@ -51,24 +61,30 @@ def refraction(ray, normal, n1, n2, critical_angle='drop'):
     cos_theta_i = torch.sum(ray * -normal, dim=1, keepdim=True)
 
     # Compute R_perp and R_para, depending on critical angle options
-    R_perp = n1/n2 * (ray + cos_theta_i * normal)
+    R_perp = n1 / n2 * (ray + cos_theta_i * normal)
 
-    if critical_angle == 'nan':
-        R_para = -torch.sqrt(1 - torch.sum(R_perp * R_perp, dim=1, keepdim=True)) * normal
-    
-    elif critical_angle == 'clamp':
-        radicand = torch.clamp(1 - torch.sum(R_perp * R_perp, dim=1, keepdim=True), min=0., max=None)
+    if critical_angle == "nan":
+        R_para = (
+            -torch.sqrt(1 - torch.sum(R_perp * R_perp, dim=1, keepdim=True)) * normal
+        )
+
+    elif critical_angle == "clamp":
+        radicand = torch.clamp(
+            1 - torch.sum(R_perp * R_perp, dim=1, keepdim=True), min=0.0, max=None
+        )
         R_para = -torch.sqrt(radicand) * normal
-    
-    elif critical_angle == 'drop':
+
+    elif critical_angle == "drop":
         radicand = 1 - torch.sum(R_perp * R_perp, dim=1, keepdim=True)
         valid = (radicand >= 0.0).squeeze()
         R_para = -torch.sqrt(radicand[valid, :]) * normal[valid, :]
         R_perp = R_perp[valid, :]
-    
+
     else:
-        raise ValueError(f"critical_angle must be one of 'nan', 'clamp', 'drop'. Got {repr(critical_angle)}.")
+        raise ValueError(
+            f"critical_angle must be one of 'nan', 'clamp', 'drop'. Got {repr(critical_angle)}."
+        )
 
     # Combine R_perp and R_para and normalize the result
     R = R_perp + R_para
-    return R / torch.norm(R, dim=1, keepdim=True)
+    return torch.div(R, torch.norm(R, dim=1, keepdim=True))
