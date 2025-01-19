63fb5935fbdc8b10c10bda58bb86ca6617bcbba3
Victor Poughon <victor.poughon@gmail.com>
2025-01-19 13:49:07 +0100

    fix types in module.py

--- src/torchlensmaker/module.py
+++ src/torchlensmaker/module.py
@@ -1,30 +1,35 @@
+import torch
 import torch.nn as nn
 
 import torchlensmaker as tlm
 
+from typing import Any, Union
+
 
 class Module(nn.Module):
     """
-    Custom nn.Module to automatically register parameters of surfaces
+    Overload of nn.Module to automatically register parameters of surfaces
 
     This is similar to how PyTorch's nn.Module automatically registers nn.Parameters
     that are assigned to it. But here, we register tlm surfaces and their inner parameters.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
-        super().__setattr__("_shapes", {})
+        self.__dict__["_shapes"] = {}
 
-    def __getattr__(self, name: str):
+    def __getattr__(self, name: str) -> Any:
         # if you get an error here, check that you called super().__init__
         # in your custom tlm.Module
         if name in self.__dict__["_shapes"]:
             return self.__dict__["_shapes"][name]
         else:
             return super().__getattr__(name)
-    
-    def __setattr__(self, name: str, value):
-        if isinstance(value, tlm.LocalSurface):
+
+    def __setattr__(
+        self, name: str, value: Union[torch.Tensor, torch.nn.modules.module.Module]
+    ) -> None:
+        if isinstance(value, tlm.surfaces.LocalSurface):
             for parameter_name, parameter in value.parameters().items():
                 self.register_parameter(name + "_" + parameter_name, parameter)
             self.__dict__["_shapes"][name] = value
