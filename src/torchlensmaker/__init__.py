07008b8af6de356c498cd0c561a687397488e8e5
Victor Poughon <victor.poughon@gmail.com>
2025-01-21 16:54:22 +0100

    add lenses

--- src/torchlensmaker/__init__.py
+++ src/torchlensmaker/__init__.py
@@ -1,25 +1,28 @@
-from torchlensmaker.full_forward import *
-
+# Core
+from torchlensmaker.physics import *
 from torchlensmaker.outline import *
 from torchlensmaker.surfaces import *
 from torchlensmaker.transforms import *
-from torchlensmaker.physics import *
-
 from torchlensmaker.intersect import *
+
+# Optics
 from torchlensmaker.optics import *
+from torchlensmaker.lenses import *
+
+# Optimization
+from torchlensmaker.parameter import parameter
+from torchlensmaker.full_forward import *
 from torchlensmaker.optimize import optimize
 
+# Viewer
 import torchlensmaker.viewer as viewer
 from torchlensmaker.viewer import ipython_show as show
 
 
-from torchlensmaker.parameter import parameter
-
 __all__ = [
     # Viewer
-    'viewer',
-    'show',
-
-    'full_forward',
-    'parameter',
+    "viewer",
+    "show",
+    "full_forward",
+    "parameter",
 ]
--- src/torchlensmaker/lenses.py
+++ src/torchlensmaker/lenses.py
@@ -67,3 +67,96 @@ def anchor_thickness(
     a2 = anchor_abs(lens[2].surface, s2_transform, anchor)
 
     return torch.linalg.vector_norm(a1 - a2)  # type: ignore
+
+
+class LensBase(nn.Module):
+    "A base class to share common lens functions"
+
+    optics: nn.Sequential
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, inputs: tlm.OpticalData) -> tlm.OpticalData:
+        return self.optics(inputs)
+
+    def inner_thickness(self) -> Tensor:
+        "Thickness at the center of the lens"
+        return anchor_thickness(self, "origin", 3, torch.float64)
+
+    def outer_thickness(self) -> Tensor:
+        "Thickness at the outer radius of the lens"
+        return anchor_thickness(self, "extent", 3, torch.float64)
+
+
+class Lens(LensBase):
+    """
+    A lens made of two refractive surfaces with different shapes
+    """
+
+    def __init__(
+        self, surface1, surface2, n, inner_thickness=None, outer_thickness=None
+    ):
+        super().__init__()
+        self.surface1, self.surface2 = surface1, surface2
+
+        thickness, anchors = lens_thickness_parametrization(
+            inner_thickness, outer_thickness
+        )
+
+        self.surface1 = tlm.RefractiveSurface(self.surface1, n, anchors=anchors)
+        self.gap = tlm.Gap(thickness)
+        self.surface2 = tlm.RefractiveSurface(
+            self.surface2, tuple(reversed(n)), anchors=tuple(reversed(anchors))
+        )
+
+        self.optics = nn.Sequential(self.surface1, self.gap, self.surface2)
+
+
+class BiLens(LensBase):
+    """
+    A lens made of two mirrored symmetrical refractive surfaces
+    """
+
+    def __init__(self, surface, n, inner_thickness=None, outer_thickness=None):
+        super().__init__()
+        self.surface = surface
+
+        thickness, anchors = lens_thickness_parametrization(
+            inner_thickness, outer_thickness
+        )
+
+        self.surface1 = tlm.RefractiveSurface(self.surface, n, anchors=anchors)
+        self.gap = tlm.Gap(thickness)
+        self.surface2 = tlm.RefractiveSurface(
+            self.surface,
+            tuple(reversed(n)),
+            scale=-1.0,
+            anchors=tuple(reversed(anchors)),
+        )
+
+        self.optics = nn.Sequential(self.surface1, self.gap, self.surface2)
+
+
+class PlanoLens(Lens):
+    """
+    A plano-convex or plano-concave lens where one surface is curved
+    as the given surface and the other surface is flat.
+
+    By default the first surface is flat and the second is curved.
+    This can be switched with the reverse argument:
+    * reverse = False (default):  The curved side is the second surface
+    * reverse = True:             The curved side is the first surface
+    """
+
+    def __init__(
+        self,
+        surface: tlm.LocalSurface,
+        n,
+        inner_thickness=None,
+        outer_thickness=None,
+        reverse=False,
+    ):
+        plane = tlm.CircularPlane(surface.outline.diameter)
+        s1, s2 = (surface, plane) if reverse else (plane, surface)
+        super().__init__(s1, s2, n, inner_thickness, outer_thickness)
