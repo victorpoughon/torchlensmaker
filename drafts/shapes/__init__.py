bc72691c40ed865b39d65bb8236a399fb974f14f
Victor Poughon <victor.poughon@gmail.com>
2025-01-22 13:45:55 +0100

    rename and delete some drafts

--- drafts/shapes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base_shape import BaseShape
-from .bezier_spline import BezierSpline
-from .circular_arc import CircularArc
-from .line import Line
-from .parabola import Parabola
-from .piecewise_line import PiecewiseLine
