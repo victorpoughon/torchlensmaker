58ea0ee82bc209005fb1088b8d78305782fad06c
Victor Poughon <victor.poughon@gmail.com>
2025-01-29 10:51:21 +0100

    plot_magnification: add color_dim

--- src/torchlensmaker/plot_magnification.py
+++ src/torchlensmaker/plot_magnification.py
@@ -1,16 +1,29 @@
 import torch
+import torch.nn as nn
 import torchlensmaker as tlm
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+from typing import Dict, Any, Optional
 
-def plot_magnification(optics, sampling):
+from torchlensmaker.viewer.render_sequence import (
+    default_colormap,
+    color_rays,
+    color_valid,
+)
+
+
+def plot_magnification(
+    optics: nn.Module,
+    sampling: Dict[str, Any],
+    color_dim: Optional[str] = None,
+    colormap: mpl.colors.LinearSegmentedColormap = default_colormap,
+):
     """
     Compute and plot magnification data for the given optical system
     The system must compute object and image coordinates
     """
-
-    # TODO add color_dim
 
     # Evaluate the optical stack
     output = optics(tlm.default_input(dim=2, dtype=torch.float64, sampling=sampling))
@@ -21,8 +34,21 @@ def plot_magnification(optics, sampling):
 
     mag, residuals = tlm.linear_magnification(T, V)
 
+    # Get color data
+    color_data = (
+        color_rays(output, color_dim, colormap).tolist()
+        if color_dim is not None
+        else color_valid
+    )
+
     fig, ax = plt.subplots(figsize=(12, 8))
-    ax.plot(T.detach().numpy(), V.detach().numpy(), linestyle="none", marker="+")
+    ax.scatter(
+        T.detach().numpy(),
+        V.detach().numpy(),
+        c=color_data,
+        marker=".",
+        s=10,
+    )
 
     X = torch.linspace(T.min().item(), T.max().item(), 50)
     ax.plot(
