bd932d1cb2d891515d1b921813a76c9079245e28
Victor Poughon <victor.poughon@gmail.com>
2025-03-10 20:06:30 +0100

    fix collision detection dev notebooks

--- src/torchlensmaker/testing/dataset_view.py
+++ src/torchlensmaker/testing/dataset_view.py
@@ -1,7 +1,8 @@
 import torch
 
 from torchlensmaker.core.transforms import IdentityTransform
-import torchlensmaker.viewer as viewer
+
+import torchlensmaker as tlm
 
 import matplotlib.pyplot as plt
 
@@ -14,22 +15,22 @@ def dataset_view(surface, P, V, rays_length=100):
     t, local_normals, valid = surface.local_collide(P, V)
     local_points = P + t.unsqueeze(-1).expand_as(V) * V
 
-    scene = viewer.new_scene("2D" if dim == 2 else "3D")
-    scene["data"].append(viewer.render_points(P, color="grey"))
-    scene["data"].extend(viewer.render_collisions(local_points, local_normals))
+    scene = tlm.viewer.new_scene("2D" if dim == 2 else "3D")
+    scene["data"].append(tlm.viewer.render_points(P, color="grey"))
+    scene["data"].extend(tlm.viewer.render_collisions(local_points, local_normals))
     
 
     rays_start = P - rays_length*V
     rays_end = P + rays_length*V
     scene["data"].append(
-        viewer.render_rays(rays_start, rays_end, layer=0)
+        tlm.viewer.render_rays(rays_start, rays_end, layer=0)
     )
 
     assert torch.all(torch.isfinite(P))
     assert torch.all(torch.isfinite(V))
 
-    scene["data"].append(viewer.render_surface(surface))
-    viewer.ipython_display(scene)
+    scene["data"].append(tlm.viewer.render_surface(surface, dim))
+    tlm.viewer.ipython_display(scene)
     #tlm.viewer.dump(scene, ndigits=2)
 
 
