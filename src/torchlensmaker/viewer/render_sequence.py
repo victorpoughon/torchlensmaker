80c5d9e0f7c2d7e5073f44c550e9a50937a7fc94
Victor Poughon <victor.poughon@gmail.com>
2025-03-19 15:17:44 +0100

    render_sequence: use output tree for making ray variables

--- src/torchlensmaker/viewer/render_sequence.py
+++ src/torchlensmaker/viewer/render_sequence.py
@@ -502,7 +502,7 @@ def render_sequence(
     )
 
     # Figure out available ray variables and their range, this will be used for coloring info by tlmviewer
-    ray_variables = RayVariables.from_optical_data(input_tree.values())
+    ray_variables = RayVariables.from_optical_data(output_tree.values())
 
     scene = viewer.new_scene("2D" if dim == 2 else "3D")
 
