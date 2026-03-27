ac19168598e832319391d6e84ddc4fbf7cc8bc51
Victor Poughon <victor.poughon@gmail.com>
2026-03-27 10:54:03 +0100

    enable legacy export for source type kernels

--- src/torchlensmaker/light_sources/source_geometry_kernels.py
+++ src/torchlensmaker/light_sources/source_geometry_kernels.py
@@ -82,6 +82,10 @@ class ObjectGeometry2DKernel(FunctionalKernel):
         "wavelength_samples": {0: "N_wavelength"},
     }
 
+    # seems to be some issue with meshgrid in some cases
+    # maybe swithing to arange would help, to be investigated
+    export_legacy = True
+
     def apply(
         self,
         tf: HomMatrix,
@@ -210,6 +214,10 @@ class ObjectGeometry3DKernel(FunctionalKernel):
         "spatial_samples": {0: "N_spatial"},
         "wavelength_samples": {0: "N_wavelength"},
     }
+
+    # seems to be some issue with meshgrid in some cases
+    # maybe swithing to arange would help, to be investigated
+    export_legacy = True
 
     def apply(
         self,
