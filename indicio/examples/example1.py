b01e0ca5956c2c268afb7d59ae0bdbdf210ea47d
Victor Poughon <victor.poughon@gmail.com>
2026-04-28 11:29:06 +0200

    move indicio to standalone repo

--- indicio/examples/example1.py
+++
@@ -1,4 +0,0 @@
-import indicio
-
-SiO2 = indicio.get_material("main", "SiO2", "Malitson")
-print(SiO2.n)
--- indicio/examples/example2.py
+++
@@ -1,5 +0,0 @@
-import indicio
-
-matches = indicio.search("H2O")
-for shelf, book, page in matches:
-    print(f"{shelf} | {book} | {page}")
